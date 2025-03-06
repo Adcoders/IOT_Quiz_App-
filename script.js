7230645d536a976e4a0b19c4e5cd6b748679b751
Adi-Pro <101057491+Adcoders@users.noreply.github.com>
2025-03-06 23:39:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -579,150 +579,133 @@ const quizData = {
   // <!-------------------------------------------- WEEK 6---------------------------------------------------------->
   "Week 6": [
     {
-    question: "Which industrial communication technology is suitable for hazardous environments?",
-    options: ["ModBus-TCP", "EtherCat", "Profibus PA", "TSN"],
-    answer: "Profibus PA"
-  },
-  {
-    question: "Which type of modem allows simultaneous bi-directional communication?",
-    options: ["Simplex", "Half-duplex", "Duplex", "Synchronous"],
-    answer: "Duplex"
-  },
-  {
-    question: "Which of the following industrial communication protocols follow a client-server architecture?",
-    options: ["MECHATROLINK", "ModBus-TCP", "Hostlink", "DeviceNet"],
-    answer: "ModBus-TCP"
-  },
-  {
-    question: "What type of tasks is enabled by Smallcell Deployment in cellular networks?",
-    options: ["Increasing dead zones", "Alleviating backhaul burden and reducing latency", "Limiting energy efficiency", "Eliminating spectrum flexibility"],
-    answer: "Alleviating backhaul burden and reducing latency"
-  },
-  {
-    question: "The DS1621 temperature sensor generates how many bits of temperature data?",
-    options: ["8 bits", "9 bits", "10 bits", "12 bits"],
-    answer: "9 bits"
-  },
-  {
-    question: "Which of the following sensors is used to measure acceleration and provides 3-axis (X, Y, Z) values?",
-    options: ["TCS3200", "LM35", "MQ-2", "ADXL335"],
-    answer: "ADXL335"
-  },
-  {
-    question: "Which sensor is used for detecting human presence based on infrared radiation?",
-    options: ["Image sensor", "Ultrasonic sensor", "PIR sensor", "Magnetostrictive sensor"],
-    answer: "PIR sensor"
-  },
-  {
-    question: "In an MOS gas sensor, what happens to the resistance in an n-type sensor when exposed to a reducing gas?",
-    options: ["Resistance remains the same", "Resistance decreases", "Resistance increases", "Resistance fluctuates randomly"],
-    answer: "Resistance decreases"
-  },
-  {
-    question: "Which of the following characteristics describes a gas sensor’s ability to detect a specific gas in the presence of other gases?",
-    options: ["Sensitivity", "Stability", "Selectivity", "Reversibility"],
-    answer: "Selectivity"
-  },
-  {
-    question: "What is the maximum data transmission range for EtherCat using optical waveguides?",
-    options: ["100 meters", "500 meters", "10 kilometers", "20 kilometers"],
-    answer: "20 kilometers"
-  },
-  {
-    question: "Which communication protocol uses Common Industrial Protocol (CIP) over Ethernet?",
-    options: ["EtherNet/IP", "Profinet", "EtherCat", "Modbus-TCP"],
-    answer: "EtherNet/IP"
-  },
-  {
-    question: "What is the total bus length supported by Interbus?",
-    options: ["7 kilometers", "10 kilometers", "13 kilometers", "24n−2 meters, where n is the number of slaves"],
-    answer: "13 kilometers"
-  },
-  {
-    question: "Which of the following is a key requirement for Industrial communication?",
-    options: ["High latency communication", "Manual device control", "Single-band connectivity", "Very low latency"],
-    answer: "Very low latency"
-  },
-  {
-    question: "How are sensors categorized based on their power requirements?",
-    options: ["Active and Passive", "Analog and Digital", "Load Resistive and Load Reactive", "Battery-Powered and Line-Powered"],
-    answer: "Active and Passive"
-  },
-  {
-    question: "Modbus is an industrial communication protocol for Fieldbuses that follows a master-slave architecture. What is the maximum number of slave devices supported by the master node in this protocol?",
-    options: ["128", "247", "256", "494"],
-    answer: "247"
-  },
-  {
-    question: "Which organization has developed the ISA 100.11a standard?",
-    options: ["Instrument Society of America", "International Society of Automation", "International Studies Association", "International Solar Alliance"],
-    answer: "International Society of Automation"
-  },
-  {
-    question: "Python is popular for embedded application development as it is a very lightweight programming language.",
-    options: ["True", "False"],
-    answer: "True"
-  },
-  {
-    question: "Adafruit provides a library to work with DHT22 Sensor.",
-    options: ["True", "False"],
-    answer: "True"
-  },
-  {
-    question: "What is the output of the following Python code?\n\n x = [4, 5, 6]\n y = [str(x[0] + 1), str(len(x) * 2) + '&Code']\n z = y[1].split('&')\n print(z[1])",
-    options: ["5", "12", "Code", "&Code"],
-    answer: "Code"
-  },
-  {
-    question: "To indicate different blocks of code, Python follows rigid indentation.",
-    options: ["True", "False"],
-    answer: "True"
-  },
-  {
-    question: "What is the output of the following line of code in Python?\n\n>>> print “Hi, Welcome to python!”",
-    options: ["Hi, Welcome to python!", "“Hi, Welcome to python!”", "Hi, Welcome to python", "None of these"],
-    answer: "Hi, Welcome to python!"
-  },
-  {
-    question: "During remote server access by a Raspberry Pi, where the Raspberry Pi acts as a client, the client needs the following?",
-    options: ["Only IP address of server", "Only port number", "Both server IP address and port number", "Client’s IP address"],
-    answer: "Both server IP address and port number"
-  },
-  {
-    question: "State whether the following command to install the PIL library is correct or not.\n\nsudo pip install pillow",
-    options: ["Correct", "Incorrect"],
-    answer: "Correct"
-  },
-  {
-    question: "What is the purpose of the 'w' mode in the open() function in Python?",
-    options: ["To read a file", "To write data to a file, overwriting existing content", "To append data to a file", "To open a file in read and write mode"],
-    answer: "To write data to a file, overwriting existing content"
-  },
-  {
-    question: "Can we configure Raspberry Pi as a File Server?",
-    options: ["Yes", "No"],
-    answer: "Yes"
-  },
-  {
-    question: "Which command is used to configure the Raspberry Pi for the camera module?",
-    options: ["sudo camera-config", "sudo raspi-config", "sudo enable-camera", "sudo pi-setup"],
-    answer: "sudo raspi-config"
-  },
-  {
-    question: "What is the final step after enabling the camera in the Raspberry Pi configuration?",
-    options: ["Restart the camera service", "Run a camera test command", "Reboot the Raspberry Pi", "Reinstall the Raspberry Pi OS"],
-    answer: "Reboot the Raspberry Pi"
-  },
-  {
-    question: "Which command exits the nano editor?",
-    options: ["Ctrl + X", "Ctrl + O", "Ctrl + K", "None of these"],
-    answer: "Ctrl + X"
-  },
-  {
-    question: "In a temperature-controlled fan system using a relay, when should the fan turn on?",
-    options: ["When the relay is manually triggered", "When the surrounding temperature is lower than a predefined threshold", "When the surrounding temperature exceeds a predefined threshold", "When the battery voltage drops below a certain level"],
-    answer: "When the surrounding temperature exceeds a predefined threshold"
-  }
+      question:
+        "Python is popular for embedded application development as it is a very lightweight programming language.",
+      options: ["True", "False"],
+      answer: "True",
+    },
+    {
+      question: "Adafruit provides a library to work with DHT22 Sensor.",
+      options: ["True", "False"],
+      answer: "True",
+    },
+    {
+      question:
+        "Consider the following piece of Python code. What is the output?\n\nx = [4, 5, 6]\ny = [str(x[0] + 1), str(len(x) * 2) + '&Code']\nz = y[1].split('&')\nprint(z[1])",
+      options: ["5", "12", "Code", "&Code"],
+      answer: "Code",
+    },
+    {
+      question:
+        "To indicate different blocks of code, Python follows rigid indentation.",
+      options: ["True", "False"],
+      answer: "True",
+    },
+    {
+      question:
+        "What is the output of the following line of code in Python?\n\n>>> print “Hi, Welcome to python!”",
+      options: [
+        "Hi, Welcome to python!",
+        "“Hi, Welcome to python!”",
+        "Hi, Welcome to python",
+        "None of these",
+      ],
+      answer: "Hi, Welcome to python!",
+    },
+    {
+      question:
+        "During remote server access by a Raspberry Pi, where the Raspberry Pi acts as a client, the client needs the following?",
+      options: [
+        "Only IP address of server",
+        "Only port number",
+        "Both server IP address and port number",
+        "Client’s IP address",
+      ],
+      answer: "Both server IP address and port number",
+    },
+    {
+      question:
+        "State whether the following command to install the PIL library is correct or not.\n\nsudo pip install pillow",
+      options: ["Correct", "Incorrect"],
+      answer: "Correct",
+    },
+    {
+      question:
+        "What is the purpose of the 'w' mode in the open() function in Python?",
+      options: [
+        "To read a file",
+        "To write data to a file, overwriting existing content",
+        "To append data to a file",
+        "To open a file in read and write mode",
+      ],
+      answer: "To write data to a file, overwriting existing content",
+    },
+    {
+      question:
+        "What will be the output of the given Python program when reading from the file?\n\nwith open('PythonProgram.txt', 'w') as file:\n    file.write('Writing data')\nwith open('PythonProgram.txt', 'r') as file:\n    f = file.read()\n    print('Reading from the file\\n')\n    print(f)",
+      options: [
+        "Writing data",
+        "Reading from the file\nWriting data",
+        "Error: File not found",
+        "None of the above",
+      ],
+      answer: "Reading from the file\nWriting data",
+    },
+    {
+      question: "Can we configure Raspberry Pi as a File Server?",
+      options: ["Yes", "No"],
+      answer: "Yes",
+    },
+    {
+      question:
+        "Which command is used to configure the Raspberry Pi for the camera module?",
+      options: [
+        "sudo camera-config",
+        "sudo raspi-config",
+        "sudo enable-camera",
+        "sudo pi-setup",
+      ],
+      answer: "sudo raspi-config",
+    },
+    {
+      question:
+        "What is the final step after enabling the camera in the Raspberry Pi configuration?",
+      options: [
+        "Restart the camera service",
+        "Run a camera test command",
+        "Reboot the Raspberry Pi",
+        "Reinstall the Raspberry Pi OS",
+      ],
+      answer: "Reboot the Raspberry Pi",
+    },
+    {
+      question: "Which command Exits the nano editor?",
+      options: ["Ctrl + X", "Ctrl + O", "Ctrl + K", "None of these"],
+      answer: "Ctrl + X",
+    },
+    {
+      question:
+        "In a temperature-controlled fan system using a relay, when should the fan turn on?",
+      options: [
+        "When the relay is manually triggered",
+        "When the surrounding temperature is lower than a predefined threshold",
+        "When the surrounding temperature exceeds a predefined threshold",
+        "When the battery voltage drops below a certain level",
+      ],
+      answer: "When the surrounding temperature exceeds a predefined threshold",
+    },
+    {
+      question:
+        "What does the following line of code do?\n\nraspistillcapture -o image.jpg",
+      options: [
+        "Captures video feed",
+        "Captures still image",
+        "Both (a) and (b)",
+        "None of these",
+      ],
+      answer: "None of these",
+    },
+    
   ],
   // <!-------------------------------------------- WEEK 7---------------------------------------------------------->
   "Week 7": [],
